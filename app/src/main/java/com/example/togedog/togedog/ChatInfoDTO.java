44b64aa639167308da948d55899d6557bf407cf5
JunSu <skyzephyr333@gmail.com>
2018-11-18 17:00:05 +0900

    ChatInfoDTO add , 시도군 스피너 완성

---
+++ app/src/main/java/com/example/togedog/togedog/ChatInfoDTO.java
@@ -0,0 +1,29 @@
+package com.example.togedog.togedog;
+
+import android.text.Editable;
+
+public class ChatInfoDTO {
+
+    public String imageUrl_chat;
+    public String chat_title;
+    public String chat_mon;
+    public String chat_tues;
+    public String chat_wed;
+    public String chat_thur;
+    public String chat_fri;
+    public String chat_sat;
+    public String chat_sun;
+    public String sta_hour;
+    public String sta_min;
+    public String fin_hour;
+    public String fin_min;
+    public String chat_do;
+    public String chat_gun;
+    public String chat_sixmon;
+    public String chat_oneye;
+    public String chat_limitso;
+    public String chat_limitjung;
+    public String chat_limitdae;
+    public String chat_unlimit;
+    public String chat_caution;
+}
